26f027d8da28c35b52ca42b83240efc8bd2bfe33
Hao <tretrau2112000@gmail.com>
2021-03-15 20:47:22 +0700

    bai tap tuan 01

---
+++ app/src/main/java/com/example/baitap_tuan01/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.baitap_tuan01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout01);
+    }
+}
